d2129172244c0153871aa1b1286125c314bc3dd7
Aysegul Aydogan <a.aydogan2018@gtu.edu.tr>
2021-10-11 22:48:00 +0300

    move function added

--- HW01/solo_test.cpp
+++ HW01/solo_test.cpp
@@ -17,6 +17,37 @@ vector< vector<string> > create_board_4()
 return foo;
 }
 
+void move(vector<vector<string>> &board, int i, int j, int direction)
+{
+    string temp;
+    temp = board[i][j];
+    if(direction == 1) //UP
+    {
+        board[i][j] = board[i-2][j];
+        board[i-1][j] = '.';
+        board[i-2][j] = temp;
+    }
+    else if(direction == 2) //DOWN
+    {
+        board[i][j] = board[i+2][j];
+        board[i+1][j] = '.';
+        board[i+2][j] = temp;
+    }
+    else if (direction == 3) //LEFT
+    {
+        board[i][j] = board[i][j-2];
+        board[i][j-1] = '.';
+        board[i][j-2] = temp;      
+    }
+    else if (direction == 4) //RIGHT
+    {
+        board[i][j] = board[i][j+2];
+        board[i][j+1] = '.';
+        board[i][j+2] = temp;        
+    }
+    
+    
+}
 void print_board(const vector<vector<string>> &board)
 {
     cout << "  ";
@@ -47,4 +78,16 @@ int main()
     vector<vector<string>> board;
     board = create_board_4();
     print_board(board);
+    while (1)
+    {
+        int i, j, direction;
+        cout<<"Enter i and enter j"<<endl;
+        cin>>i>>j;
+        cout<<"Enter direction"<<endl;
+        cin>>direction;
+        move(board,i,j,direction);
+        cout<<"-----------------"<<endl;
+        print_board(board);
+    }
+    
 }
