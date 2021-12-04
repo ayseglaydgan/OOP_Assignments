f12a9f3c38aee88d83cee3f11b070f54059fa1d7
Aysegul Aydogan <a.aydogan2018@gtu.edu.tr>
2021-12-04 15:15:41 +0300

    Add hw04 files

---
+++ HW04/DayOfYearSet.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class DayOfYearSet
+{
+public:
+
+
+private:
+    
+};
+
+
+
+
---
+++ HW04/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include "DayOfYearSet.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Domates" << endl;
+}
