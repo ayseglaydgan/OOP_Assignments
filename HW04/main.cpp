eca841ec1ca6aeeb4a03d75264f0e4b772890fc2
Aysegul Aydogan <a.aydogan2018@gtu.edu.tr>
2021-12-04 15:37:20 +0300

    Add factorial fucn

--- HW04/main.cpp
+++ HW04/main.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+// factorial
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
 int main()
 {
     cout << "Domates" << endl;
